52972b8b009022e9f4552fc92fceeb1f56225e80
ericcalvo <ecalvog@fp.insjoaquimmir.cat>
2020-10-05 19:55:41 +0200

    exercici5

--- eric/UF1A2_CalvoEric/calendari.php
+++ eric/UF1A2_CalvoEric/calendari.php
@@ -5,15 +5,13 @@
 </head>
 <body>
     <?php
-        $dia = 1;
-        $mes=date("F");
-        $any=date("Y");
-        $numero=date('w', mktime(0, 0, 0, $dia, $mes, $any));
-        echo "<table border=1><tr><th colspan =7>$mes, $any</th></tr>";
-        echo "<tr><th>Dilluns</th><th>Dimarts</th><th>Dimecres</th><th>Dijous</th><th>Divendres</th><th>Dissabte</th><th>Diumenge</th></tr>";
 
-        
-        echo "</table>";
+    for($i=1; $i<31; $i++){
+        echo("$i ");
+        if($i%7==0){
+            echo "<br>";
+        }
+    }
 
     ?>
 
