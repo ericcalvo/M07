43989f2b5071f5cc34f1f8b09c54bdc9950990df
ericcalvo <ecalvog@fp.insjoaquimmir.cat>
2020-10-07 20:18:54 +0200

    calendari

--- eric/UF1/a2/calendari.php
+++ eric/UF1/a2/calendari.php
@@ -1,18 +1,38 @@
-<!DOCTYPE html>
-<head>
-    <meta charset="UTF-8">
-    <title>Calendari</title>
-</head>
-<body>
-    <?php
+<?php
+$mes=date("n");
+$any=date("Y");
+$primerDia=date("w",mktime(0,0,0,$mes,1,$any));
+$ultimDia=date("d",(mktime(0,0,0,$mes+1,1,$any)-1));
 
-    for($i=1; $i<31; $i++){
-        echo("$i ");
-        if($i%7==0){
-            echo "<br>";
+$dias=array("Dilluns","Dimarts","Dimecres","Dijous","Divendres","Dissabte","Diumenge");
+
+// Indica els dies de la semana i crea la taula.
+echo "<table border=2>";
+for ($i=0;$i<=6;$i++){
+    echo "<td> $dias[$i]</td>";
+};
+
+$posicio=1;
+$dia=1;
+//Comença el programa des del primer dia al ultim del mes.
+while ($dia<=$ultimDia){
+    echo "<tr>";
+    $comptador=0;
+
+    //Per que no pasi de 7 espais en la taula.
+    while ($dia<=$ultimDia && $comptador<7){
+        //Posicionament dels dias
+        while ($posicio<$primerDia){   
+            echo "<td></td>";
+            $posicio++;
+            $comptador++;
         }
+        //Escriu els dies en la taula.
+        echo "<td>$dia</td>"; 
+        $dia++;
+        $comptador++;
     }
-
-    ?>
-
-</body>
+    echo "</tr>";
+}
+echo "</table>"
+?>
